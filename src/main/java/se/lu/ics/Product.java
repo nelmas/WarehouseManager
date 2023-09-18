0a5d6fd6494d3538fe6a35a01684edff4a0d8719
nelmas <an3744ma-s@student.lu.se>
2023-09-18 12:27:38 +0200

    created Product class

---
+++ src/main/java/se/lu/ics/Product.java
@@ -0,0 +1,45 @@
+package se.lu.ics;
+
+public class Product {
+    // Attributes
+    private int id;
+    private String name;
+    private String category;
+
+    // Constructor
+    public Product(int id, String name, String category) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+    }
+
+    // Getter methods
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    // Setter methods
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+}
+
+
+
