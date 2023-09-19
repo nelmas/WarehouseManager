09b523f9c18e6cdbcbdaad1e1b145b8514ba0ada
BjoernKjellberg <118634340+BjoernKjellberg@users.noreply.github.com>
2023-09-19 10:55:13 +0200

    Provar_att_committa

--- src/main/java/se/lu/ics/Main.java
+++ src/main/java/se/lu/ics/Main.java
@@ -3,6 +3,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
+        /* dev_bjoern bara testar en commit, men vill då alltså INTE göra något som påverkar så skriver bara lite text! */
        
 
         
