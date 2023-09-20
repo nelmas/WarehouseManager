ed2f10e59a80543a6c1f05286e683bdd09f2a198
BjoernKjellberg <118634340+BjoernKjellberg@users.noreply.github.com>
2023-09-20 15:16:29 +0200

    Sup.controller, App window size fix, sup.app fix

--- src/main/java/se/lu/ics/controllers/SupplierController.java
+++ src/main/java/se/lu/ics/controllers/SupplierController.java
@@ -1,13 +1,16 @@
 package se.lu.ics.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-
+import javafx.scene.control.cell.PropertyValueFactory;
 import se.lu.ics.models.Supplier;
+import se.lu.ics.data.SupplierDAO;
+import se.lu.ics.data.WarehouseDAO;
 import se.lu.ics.models.Product;
 import se.lu.ics.models.Stored;
 import se.lu.ics.models.Warehouse;
@@ -40,11 +43,36 @@ public class SupplierController {
 
     @FXML private TableColumn<Supplier, String> column_supplierName;
 
-    @FXML private TableColumn<Supplier, Double> column_supplierCapacity;
+    @FXML private TableColumn<Supplier, String> column_supplierAddress;
+
+    @FXML private TableColumn<Supplier, String> column_supplierEmail;
 
     @FXML private Button button_addSupplier;
 
     @FXML private Button button_removeSupplier;
+
+    //method for initializing the tableview
+    public void initialize() {        
+        column_supplierId.setCellValueFactory(new PropertyValueFactory<Supplier, String>("supplierId"));
+        column_supplierName.setCellValueFactory(new PropertyValueFactory<Supplier, String>("supplierName"));
+        column_supplierAddress.setCellValueFactory(new PropertyValueFactory<Supplier, String>("address"));
+        column_supplierEmail.setCellValueFactory(new PropertyValueFactory<Supplier, String>("email"));
+
+        tableView_supplier.setItems(SupplierDAO.getSuppliers());
+    }
+
+    //Search for supplier by id method 
+    public void supplierSearchById () {
+    String supplierId = textField_supplierSearchField.getText().toLowerCase(); 
+    for (Supplier supplier : SupplierDAO.getSuppliers()) {
+        tableView_supplier.getSelectionModel().select(supplier); 
+        if (supplier.getSupplierId().toLowerCase().equals(supplierId)) {
+                    
+            }
+    
+        }  
+
+    }
 
     public void button_removeSupplier() {
         //TODO
