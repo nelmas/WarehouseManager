46ea2d1be7b8a18fb270d19e1af362282139e1ab
nelmas <an3744ma-s@student.lu.se>
2023-09-24 14:09:08 +0200

    Tables now visible by SQL-query

--- src/main/java/se/lu/ics/controllers/MetaDataController.java
+++ src/main/java/se/lu/ics/controllers/MetaDataController.java
@@ -1,5 +1,59 @@
 package se.lu.ics.controllers;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import se.lu.ics.models.Product;
+import se.lu.ics.models.Stored;
+import se.lu.ics.models.Supplier;
+import se.lu.ics.data.MetadataDAO;
+import se.lu.ics.data.ProductDAO;
+import se.lu.ics.data.StoredDAO;
+import se.lu.ics.data.SupplierDAO;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class MetaDataController {
+
+    @FXML
+    private TableColumn<Product, String> columnProductId;
+    @FXML
+    private TableColumn<Product, String> columnProductName;
+    @FXML
+    private TableColumn<Product, String> columnProductCategory;
+    @FXML
+    private TableColumn<Product, Supplier> columnProductSupplierId;
+    @FXML
+    TableColumn<String, String> tableNameColumn = new TableColumn<>("Table Name");
+    @FXML
+    private TableView<Product> productTable;
+    @FXML
+    private TableView<String> metadataNamesOfAllTables;
+    @FXML
+    private TableView<String> columnNamesTable;
+    @FXML
+    private TableView<String> columnTypesTable;
+
+
+    public void initialize() {
+        // Set up the columns in the table
+        tableNameColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue()));
+
+        // Retrieve table names from the MetadataDAO
+        ObservableList<String> tableNames = MetadataDAO.getAllTableNames();
+
+        // Populate the TableView with table names
+        metadataNamesOfAllTables.setItems(tableNames);
     
+    }
 }
--- src/main/java/se/lu/ics/data/MetadataDAO.java
+++ src/main/java/se/lu/ics/data/MetadataDAO.java
@@ -1,6 +1,32 @@
 package se.lu.ics.data;
 
+import java.sql.*;
+
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+
 public class MetadataDAO {
-    
-    
+
+    public MetadataDAO(String tableName, String tableRows) {
+
+    }
+
+    public static ObservableList<String> getAllTableNames() {
+        ObservableList<String> tableNames = FXCollections.observableArrayList();
+        String query = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES";
+
+        try (Connection connection = ConnectionHandler.getConnection();
+         Statement statement = connection.createStatement();
+         ResultSet resultSet = statement.executeQuery(query)) {
+
+        while (resultSet.next()) {
+            String tableName = resultSet.getString("TABLE_NAME");
+            System.out.println("Table name: " + "tableName");
+            tableNames.add(tableName);
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    return tableNames;
+    }
 }
